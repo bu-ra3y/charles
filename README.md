bb84bc2841558015a8246fb3ba0f2ce54f58cc53
Will Barley <will.barley@gmail.com>
2019-10-15 14:16:47 -0400

    ADD Charles river webapp

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # charles
 Charles River Watershed Association
+
+# Deploy the Web App
+Deployed on AWS Elastic Beanstalk.
+
+Lives in `web/app.py`
+
+## Requirements.txt
+The packages available to the app on AWS Elastic Beanstalk is controlled by requirements.txt
+
+## Install and configure `awsebcli`
+`pip install awsebcli`
+
+go to project directory and `eb init` (to connect this directory to AWS EB)
+
+then `eb create` to create an environment (e.g. Dev vs Prod)
+
+update WSGI path to point to the app:
+`eb console` to jump on to the AWS console > Configuration > update WSGI path to:
+`app.py` 
+
+
+## Deploy
+Navigate to the repository directory and run `eb deploy --label <version_name>`.
+This will deploy the files that are currently in the directory, minus those listed in `.ebignore`.   
+The deployment will not related to a specific git commit (for explanation see the `.ebignore` section below).
+
+
+#### .ebignore
+Uses the .ebignore file to not exclude most of the repository when deploying.
+awsebcli normally deploys `git HEAD`, but when using .ebignore, it does not and instead deploys whatever files are on the filesystem. 
+
+#### requirements.txt
+Elastic Beanstalk needs a `requirements.txt` in order to install the right libraries.
+
---
+++ web/app.py
@@ -0,0 +1,234 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+from datetime import time, datetime
+
+from dash.dependencies import Output, Input
+from plotly import graph_objects as go
+from plotly.graph_objs.layout import legend
+
+
+def load_sites():
+    sites_file = '/Users/will/PycharmProjects/charles/test_data/Monitoring_Sites.csv'
+    sites = pd.read_csv(sites_file)
+    # sites[['Site_Name', 'Town', 'Latitude_DD', 'Longitude_DD', 'Site_Description']]
+    return sites
+
+
+def load_results(sites):
+    results_file = '/Users/will/PycharmProjects/charles/test_data/Results.csv'
+    r = pd.read_csv(results_file, low_memory=False)
+
+    # Accepted results only
+    r = r[r['QAQC_Status'].str.lower().str.contains('accepted', na=False)]
+
+    # Just a few columns
+    r = r[['Date_Collected', 'Time_Collected', 'Site_ID', 'Reporting_Result', 'Analytical_Method_ID']]
+
+    # Fix the Collection Date
+    r['Date_Collected'] = pd.to_datetime(r['Date_Collected']).dt.date
+
+    # Fix the Time
+    r['Time_Collected'] = pd.to_datetime(r['Time_Collected'], errors='coerce').dt.time
+    r['Time_Collected'].fillna(time(), inplace=True)  # Fill in missing times with 00:00
+
+    # Create a combined Datetime column
+    r['datetime'] = r.apply(lambda x: pd.datetime.combine(x['Date_Collected'], x['Time_Collected']), 1)
+
+    # Drop unused columns
+    r = r.drop(['Time_Collected'], 1)
+
+    # Results as numbers
+    r['Reporting_Result'] = pd.to_numeric(r['Reporting_Result'], errors='coerce')
+    r = r[r['Reporting_Result'].notnull()]  # Filter out results that are NAN
+
+    # Join with Sites DATA to get Site Name, lat, lon
+    columns = ['Site_ID', 'Date_Collected', 'datetime', 'Reporting_Result', 'Analytical_Method_ID', 'Site_Name', 'Town', 'Latitude_DD', 'Longitude_DD', ]
+    r = r.merge(right=sites, how='left', on='Site_ID')[columns]
+
+    # Drop results with missing DATA in one of the required columns
+    r = r.dropna(subset=['Latitude_DD', 'Longitude_DD', 'Reporting_Result', 'Date_Collected', 'datetime', 'Site_ID', 'Analytical_Method_ID'])
+
+    # TODO: limit to the 37 fixed sites
+    # sites[sites['Site_ID'].isin(r['Site_ID'].unique())]
+
+    # Sort by datetime
+    r.sort_values('datetime', inplace=True)
+
+    # See what we have
+    return r
+
+
+def load_methods():
+    methods_file = '/Users/will/PycharmProjects/charles/test_data/Analytical_Methods.csv'
+    m = pd.read_csv(methods_file)
+    return m
+
+
+def ecoli_methods(methods):
+    e_m = methods[methods['Parameter'] == 'Escherichia coli']
+    e_m = e_m['Analytical_Method_ID']
+    return e_m
+
+
+def ecoli_data():
+    r = load_results(load_sites())
+    e_m = ecoli_methods(load_methods())
+    ecoli = r[r['Analytical_Method_ID'].isin(e_m)].copy()
+
+    # Drop unneeded method column
+    ecoli = ecoli.drop(['Analytical_Method_ID'], 1)
+
+    # Categorize Values into Severity Levels, thresholds are 126 and 630
+    levels = ['Swimming', 'Boating', 'Danger']
+    ecoli['severity'] = pd.cut(ecoli['Reporting_Result'], [0, 126, 630, 100000], right=False, labels=levels)
+
+    color_for_category = {
+        'Swimming': 'green',
+        'Boating' : 'yellow',
+        'Danger'  : 'blue',
+    }
+    ecoli['colors'] = [color_for_category[c] for c in ecoli['severity']]
+
+    return ecoli
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+DATA = ecoli_data()
+# print(DATA)
+# print(DATA.columns)
+
+
+def get_dates(data):
+    return sorted(list(data['Date_Collected'].unique()), reverse=True)
+
+
+def get_parameters():
+    methods = load_methods()
+    parameters = [p for p in methods.Parameter.unique() if isinstance(p, str)]
+    parameters = sorted(parameters)
+    print(parameters)
+    return parameters
+
+
+app.layout = html.Div(children=[
+    html.H1(children='E. coli Levels'),
+    html.Div([
+        dcc.Dropdown(
+            id='parameter',
+            options=[{'label': i, 'value': i} for i in get_parameters()],
+            value='Escherichia coli',
+            placeholder='Choose a parameter',
+            className="five columns",
+        ),
+        dcc.Dropdown(
+            id='date',
+            options=[{'label': i, 'value': i} for i in get_dates(DATA)],
+            value=get_dates(DATA)[0],
+            placeholder='Choose a date',
+            className="three columns",
+        ),
+    ],
+        className='row'
+    ),
+    dcc.Graph(id='map'),
+    dcc.Graph(id='chart'),
+])
+
+@app.callback(
+    Output('chart', 'figure'),
+    [Input('map', 'clickData'),
+     Input('date', 'value')]
+)
+def update_chart(selected_site, date_string):
+    if not selected_site:
+        return go.Figure()
+
+    name = selected_site.get('points')[0].get('text')
+    data = DATA[DATA['Site_Name'] == name]
+    date = string_date_to_date(date_string)
+    print(data)
+    print(data.columns)
+    return go.Figure(
+        data=[
+            go.Scatter(
+                x=(date, date),
+                y=(0, 20000),
+                name=date_string,
+                mode='lines',
+                # text=data.Site_Name if not data.empty else list(),
+                # hoverinfo='text',
+                showlegend=False,
+            ),
+            go.Scatter(
+                x=data.datetime,
+                y=data.Reporting_Result,
+                name='Escherichia coli',
+                line_color='grey',
+                # text=data.Site_Name if not data.empty else list(),
+                # hoverinfo='text',
+            ),
+        ],
+        layout=go.Layout(
+            title=name,
+            yaxis=dict(
+                range=[0, 20000],
+            ),
+        ),
+    )
+
+def string_date_to_date(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d').date()
+
+
+@app.callback(
+    Output('map', 'figure'),
+    [Input('date', 'value'),
+     Input('parameter', 'value'),
+     ],)
+def update_map(date, parameter):
+    mapbox_access_token = None
+
+    if date:
+        data = DATA[DATA['Date_Collected'] == string_date_to_date(date)]
+    else:
+        data = pd.DataFrame()
+
+    fig = go.Figure(
+        go.Scattermapbox(
+            lat=data.Latitude_DD if not data.empty else list(),
+            lon=data.Longitude_DD if not data.empty else list(),
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=14,
+                color=data.colors if not data.empty else list(),
+            ),
+            text=data.Site_Name if not data.empty else list(),
+            hoverinfo='text',
+        )
+    )
+
+    fig.update_layout(
+        hovermode='closest',
+        title="Sites",
+        margin={"r":0,"t":0,"l":0,"b":0},
+        mapbox=go.layout.Mapbox(
+            accesstoken=mapbox_access_token,
+            style="carto-positron",
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=42.3,
+                lon=-71.2
+            ),
+            zoom=9,
+        )
+    )
+
+    return fig
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
